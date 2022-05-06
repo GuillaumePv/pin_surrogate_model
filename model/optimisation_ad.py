4c310adf1bcdf537a83699f176478efefe99586f
Guillaume Pavé <guillaumepave@gmail.com>
2022-05-06 08:54:46 +0200

    test new to optimize

--- model/optimisation_ad.py
+++ model/optimisation_ad.py
@@ -52,13 +52,19 @@ class Optimizer:
         x_init = pd.DataFrame([0.5,0.5,250,250,250,250,250]).transpose()
         bounds = tf.fill((1, len(x_init)), self.pivot)
         bound_cost = tf.constant(100.0, dtype=tf.float64)
-        number_echantillon = 200
 
         test_value = self.X.iloc[:1,:]
+        print(test_value.shape)
         Y = test_value["MLE"]
         x_params = test_value.iloc[:,:-1]
         
         buy_and_sell = test_value.iloc[:,-3:-1]
+
+        # pas sûr que ça soit utile
+        mean_buy = np.mean(buy_and_sell["buy"])
+        mean_sell = np.mean(buy_and_sell["sell"])
+        u = np.abs(mean_buy - mean_sell)
+
         buy_and_sell = (buy_and_sell - self.means)/self.std
         buy_and_sell = tf.convert_to_tensor(buy_and_sell.values)
         buy_and_sell = tf.cast(buy_and_sell,tf.double)
@@ -67,13 +73,22 @@ class Optimizer:
         # print(params)
         print("initial value")
         print(x_params)
-        print(-Y)
+        print(Y)
         
+        test_try = 0
         def func(x_params):
             #print(x_params)
-            test_value = tf.concat([x_params, buy_and_sell],axis=1)
+
+            # x_array = np.array([x_params.numpy(),x_params.numpy(),x_params.numpy(),x_params.numpy(), x_params_est])
+            # x_init = pd.DataFrame(x_array)
+            # # x_init = tf.convert_to_tensor(x_init, tf.double)
+            # x_init = tf.convert_to_tensor(x_init,tf.double)
+
+            # test_tf = tf.constant([x_params.numpy(), x_params.numpy(), x_params.numpy(),x_params.numpy()],dtype=tf.double)
+            # #print(test_tf)
+            # test_value = tf.concat([x_init, buy_and_sell],axis=1)
             # print(test_value)
-            pred = self.c_model.model(test_value)
+            pred = self.c_model.model(tf.reshape(x_params,[1,7]))
             
         
             v_call = tf_loss(pred,Y)
@@ -83,7 +98,7 @@ class Optimizer:
             return tf.reduce_mean(v_call) + tf.reduce_mean(bnd) # converge ok 
             # return tf.reduce_mean(v_call) # not converge !!!
             ## Use that to minimize MLE
-            # return tf.reduce_mean(-pred[0][0]) + bnd
+            # return -tf.reduce_sum(pred) + tf.reduce_mean(bnd)
 
         ## need to convert to tensor ##
         def func_g(x_params):
@@ -94,29 +109,40 @@ class Optimizer:
             grads = tape.gradient(loss_value,[x_params])
             return loss_value, grads[0]
 
-        x_init = pd.DataFrame([0.5,0.5,250,250,250]).transpose()
+        eb0, es0, u0 = np.random.poisson([mean_buy,mean_sell,u])
+        x_params_est = [0.00001,0.00001,0.00001,0.00001,0.00001,0.00001,0.00001]
+        x_array = np.array([x_params_est,x_params_est,x_params_est,x_params_est, x_params_est])
+        x_init = pd.DataFrame(x_array)
         # x_init = tf.convert_to_tensor(x_init, tf.double)
-        x_init = tf.convert_to_tensor(x_init)
+        x_init = tf.transpose(tf.convert_to_tensor(x_params_est,tf.double))
         print(x_init)
+        #print(x_init)
+        test_concat = buy_and_sell
+        #print(test_concat)
+        
         s = time.time()
         soln = tfp.optimizer.lbfgs_minimize(func_g, x_init, max_iterations=50, tolerance=1e-60)
-        print(soln.converged)
+        print(f"It takes ")
+        print("Converge ??")
+        print(soln.converged.numpy())
         soln_time = np.round((time.time() - s) / 60, 2)
         pred_par = soln.position.numpy()
         obj_value = soln.objective_value.numpy()
-        print(pred_par[0])
+        print(pred_par)
         # print(buy_and_sell.numpy()[0])
-        final_value = np.concatenate([pred_par[0],buy_and_sell.numpy()[0]],axis=0)
+        #final_value = np.concatenate([pred_par,buy_and_sell.numpy()],axis=1)
         print("=== final value ===")
-        print(final_value)
+        #print(final_value)
         
+        print("objective func")
         print(obj_value)
-        # print(self.c_model.unnormalize(pred_par[0])[0])
-        res = self.c_model.unnormalize(final_value)[0]
-        print(res)
-        res = res.T
-        PIN = (res['alpha']*res['mu'])/((res['alpha']*res['mu'])+res['epsilon_b']+res['epsilon_s'])
-        print(PIN)
+        
+        print(self.c_model.unnormalize(pred_par[0])[0])
+        # res = self.c_model.unnormalize(final_value)[0]
+        # print(res)
+        # res = res.T
+        # PIN = (res['alpha']*res['mu'])/((res['alpha']*res['mu'])+res['epsilon_b']+res['epsilon_s'])
+        # print(PIN)
         # score=self.c_model.score(self.c_model.unnormalize(pred_par)[0],Y)
         
 
