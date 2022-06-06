382293daa061243c1be58afcba1b0cac05858bc4
Guillaume Pavé <guillaumepave@gmail.com>
2022-06-06 18:05:40 +0200

    finish PIN simulation code

--- pin_example_estimation.py
+++ pin_example_estimation.py
@@ -1,10 +1,17 @@
 from model.deepsurrogate import DeepSurrogate
-
+from tqdm import tqdm
 deepsurrogate = DeepSurrogate()
 
-df_b_s = deepsurrogate.X[['buy','sell']]
-df_b_s = df_b_s.head(3)
+df_pin = deepsurrogate.X[['buy','sell']]
+df_pin = df_pin.head(3)
 
-for i in range(df_b_s.shape[0]):
-    pin = deepsurrogate.get_pin(df_b_s.loc[i].values)
-    print(pin)
+PIN_values = []
+for i in tqdm(range(df_pin.shape[0])):
+    pin = deepsurrogate.get_pin(df_pin.loc[i].values)
+    PIN_values.append(pin)
+
+df_pin['PIN'] = PIN_values
+print("=== Save simulation result ===")
+df_pin.to_latex("./results/table/simulation_result_pin.tex",index=False)
+df_pin.to_csv("./results/table/simulation_result_pin.csv",index=False)
+print(df_pin)
